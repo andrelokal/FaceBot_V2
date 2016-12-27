29e39853b1994dd141032314de456f8becedff33
Andre L. Martos <andremartos@movida.com.br>
2016-12-27 17:14:57 -0200

    #valida cpf

--- util/validators.php
+++ util/validators.php
@@ -12,8 +12,51 @@ function validarTeste($data){
 }
 */
 
-function passo1($dados){
-    return array("success"=>false,"msg"=>"deu ruim!!!");    
+function passo7($cpf = null) {
+
+    // Verifica se um número foi informado
+    if(empty($cpf)) {
+        return array("success"=>false,"msg"=>"cpf não informado");
+    }
+
+    // Elimina possivel mascara
+    $cpf = ereg_replace('[^0-9]', '', $cpf);
+    $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
+
+    // Verifica se o numero de digitos informados é igual a 11 
+    if (strlen($cpf) != 11) {
+        return array("success"=>false,"msg"=>"quantidade de caracteres não corresponde ao de um cpf valido");
+    }
+    // Verifica se nenhuma das sequências invalidas abaixo 
+    // foi digitada. Caso afirmativo, retorna falso
+    else if ($cpf == '00000000000' || 
+    $cpf == '11111111111' || 
+    $cpf == '22222222222' || 
+    $cpf == '33333333333' || 
+    $cpf == '44444444444' || 
+    $cpf == '55555555555' || 
+    $cpf == '66666666666' || 
+    $cpf == '77777777777' || 
+    $cpf == '88888888888' || 
+    $cpf == '99999999999') {
+        return array("success"=>false,"msg"=>"cpf inválido");
+        // Calcula os digitos verificadores para verificar se o
+        // CPF é válido
+    } else {   
+
+        for ($t = 9; $t < 11; $t++) {
+
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf{$c} * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf{$c} != $d) {
+                return array("success"=>false,"msg"=>"cpf inválido");
+            }
+        }
+
+        return true;
+    }
 }
 
 
